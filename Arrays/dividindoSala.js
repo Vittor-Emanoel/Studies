195d90c66d22e6c7d9a156af502a7c1880899d8f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-07 14:34:46 -0300

    método slice - fatia o array

---
+++ Arrays/dividindoSala.js
@@ -0,0 +1,28 @@
+const alunos = [
+  'João',
+  'Juliana',
+  'Ana',
+  'Caio',
+  'Lara',
+  'Marjorie',
+  'Guilherme',
+  'Aline',
+  'Fabiana',
+  'Andre',
+  'Carlos',
+  'Paulo',
+  'Bia',
+  'Vivian',
+  'Isabela',
+  'Vinícius',
+  'Renan',
+  'Renata',
+  'Daisy',
+  'Camilo',
+]
+
+// não altera o array original
+//do indice 0 ao 9
+const sala1 = alunos.slice(0, 10)
+
+console.log(sala1)
