1eadbf61aaf7b1130b1e6ab8a37204397b6c6b47
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-07 15:56:37 -0300

    emendar arrays

---
+++ Arrays/listaDuasDimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio']
+const medias = [10, 8, 7.5, 9]
+
+const listaDeALunoEmedias = [alunos, medias]
+
+console.log(
+  `A Aluna da posição 1 da lista de alunos é ${listaDeALunoEmedias[0][1]} 
+  A nota dessa Aluna é ${listaDeALunoEmedias[1][1]}
+  
+  `
+)
