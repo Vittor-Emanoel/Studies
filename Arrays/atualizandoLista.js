574895f7741a06a75e5121bea9b7b9193cb1be8f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-07 14:56:48 -0300

    método splice - emenda o array

---
+++ Arrays/atualizandoLista.js
@@ -0,0 +1,7 @@
+const nomes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
+
+//Emendar - remover
+//o Splice modifica o array - diferente do splicee
+nomes.splice(1, 2, 'Rodrigo')
+//colocou no lugar da ana
+console.log(nomes)
