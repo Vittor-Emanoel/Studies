4c964c98e0d16e8542ba2da4fa4092b69c209d11
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-09 17:18:48 -0300

    for clássico

---
+++ Arrays/for.js
@@ -0,0 +1,12 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+//3 expressões
+
+//1 - inicializador, é executada apenas uma única vez
+//2 - condição de execução
+//3 - condição de parada
+
+//verifica se a condição é verdadeira, quando ficar false ela quebra
+for (let indice = 0; indice < 6; indice++) {
+  console.log('oi')
+}
